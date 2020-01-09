d50854a738fb4ebe2b87c179ce3d334b71f12bc9
blazerlee <isc.danivalencia@gmail.com>
2020-01-09 13:22:35 -0600

    cambio hecho por ryu

--- plsql.sql
+++ plsql.sql
@@ -5,10 +5,13 @@ declare
 p_letter  varchar2(20) :='Hola mundo';
 p_letter2 varchar2(40) :=' y Hola Daniel';
 v_letter varchar2(50);
+v_date date;
 
 begin
 
 ----Este es mi comentario
 select p_letter ||p_letter2 into v_letter from dual;
+select sysdate into v_date from dual;
 dbms_output.put_line(v_letter);
+dbms_output.put_line(v_date);
 end;
